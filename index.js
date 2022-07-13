63aff90b24500b2e81519b26962833d8694bc80a
Vadim <www.vadim.2@mail.ru>
2022-07-13 16:34:03 +0300

    start burgerMenu

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log('10 \n' + '20 \n' + '48 \n' + '12 \n' + '5+5+5+5 \n'+ ' = 110 ');
+document.getElementById("menu").onclick = function() {open()};
+
+function open() {
+    document.getElementById("navigationMenu").classList.toggle("show");
+}
