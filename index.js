c445920687b90339166e8a4d5599d566da350d87
Vadim <www.vadim.2@mail.ru>
2022-07-13 18:00:58 +0300

    fix color burgerMenu

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ document.getElementById("menu").onclick = function() {open()};
 document.getElementById("close").onclick = function() {close()};
 
 function open() {
+    document.getElementById("menu").style.display="none";
     document.getElementById("navigationMenu").classList.toggle("show");
 }
 function close() {
     document.getElementById("navigationMenu").classList.toggle("closeNavi");
+    document.getElementById("menu").style.display="block";
 }
