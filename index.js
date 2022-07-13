fd668668dfc5593df7d6cfbb02020d2863591afc
Vadim <www.vadim.2@mail.ru>
2022-07-13 18:01:15 +0300

    fix color burgerMenu

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log('10 \n' + '20 \n' + '48 \n' + '12 \n' + '5+5+5+5 \n'+ ' = 110 ');
 document.getElementById("menu").onclick = function() {open()};
 document.getElementById("close").onclick = function() {close()};
 
