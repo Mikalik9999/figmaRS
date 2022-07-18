a3cb55ed5d6ee94f4496b6b09aea72d748409c18
Vadim <98332871+Mikalik9999@users.noreply.github.com>
2022-07-18 14:46:29 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.getElementById("menu").onclick = function() {open()};
 document.getElementById("close").onclick = function() {close()};
+document.getElementById("nextRight").onclick = function() {right()};
+document.getElementById("nextLeft").onclick = function() {left()};
 document.onclick = function (e) {
     if (e.target.className !== "menuBurger" ) {
         close();
@@ -10,6 +12,7 @@ function open() {
     document.getElementById("menu").style.display="none";
     document.getElementById("navigationMenu").classList.add("show");
 }
+
 function close() {
     document.getElementById("navigationMenu").classList.add("transformation");
     function close() {
@@ -18,6 +21,41 @@ function close() {
         document.getElementById("navigationMenu").classList.remove("transformation")
     }
     setTimeout(close, 1000);
+}
+
+function right () {
+    if (document.getElementById("spain").innerText === 'JAPAN')
+    {
+        document.getElementById("country").classList.add("showUsa");
+        document.getElementById("spain").innerText= 'USA';
+        document.getElementById("nextSpain").style.background = "rgba(242, 120, 92, 0.5)";
+        document.getElementById("nextUsa").style.background = "#F2785C";
+        document.getElementById("nextJapan").style.background = "rgba(242, 120, 92, 0.5)";
+    } else if (document.getElementById("spain").innerText === 'SPAIN'){
+    document.getElementById("country").classList.add("showJapan");
+    document.getElementById("spain").innerText= 'JAPAN';
+    document.getElementById("nextSpain").style.background = "#F2785C";
+    document.getElementById("nextJapan").style.background = "rgba(242, 120, 92, 0.5)";
+    }
+}
+
+function left () {
+    if (document.getElementById("spain").innerText === 'JAPAN')
+    {
+        document.getElementById("country").classList.remove("showUsa");
+        document.getElementById("country").classList.remove("showJapan");
+        document.getElementById("spain").innerText= 'SPAIN';
+        document.getElementById("nextSpain").style.background = "rgba(242, 120, 92, 0.5)";
+        document.getElementById("nextJapan").style.background = "#F2785C";
+        document.getElementById("nextUsa").style.background = "rgba(242, 120, 92, 0.5)";
+    } else if (document.getElementById("spain").innerText === 'USA'){
+    document.getElementById("country").classList.remove("showUsa");
+    document.getElementById("country").classList.add("showJapan");
+    document.getElementById("spain").innerText= 'JAPAN';
+    document.getElementById("nextSpain").style.background = "#F2785C";
+    document.getElementById("nextJapan").style.background = "rgba(242, 120, 92, 0.5)"
+    document.getElementById("nextUsa").style.background = "rgba(242, 120, 92, 0.5)";
+    }
 }
 
 
